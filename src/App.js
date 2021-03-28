e6727f76dc24ea1d12de268abbce24df503a9496
samar-18 <benzeinebsamar@gmail.com>
2021-03-28 17:19:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from "react";
+import imageInSrc from './imageInSrc.png';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+  <>
+  <div className="principal" >
+    <h1 class="title-red">Samar Ben Zeineb</h1>
+
+  
+    <img src="/imageInPublic.png"  alt="myprofile" className="myprofile" />
+
+    <img className="my-profile" src={imageInSrc} />
+
+    <video className="VI" src="/react.mp4" />
+    
+    
+</div>
+    </>
+
+  )
 }
 
 export default App;
